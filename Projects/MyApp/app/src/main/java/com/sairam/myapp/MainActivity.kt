b77d6e3100560387114cf3830c632038b33cca62
pavan <sairam.g581@gmail.com>
2020-06-28 12:24:52 +0530

    Adding examples...

--- Projects/MyApp/app/src/main/java/com/sairam/myapp/MainActivity.kt
+++ Projects/MyApp/app/src/main/java/com/sairam/myapp/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.sairam.myapp
 
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.textView)
+        text.text = getLocalIpAddress()
+
     }
+
+    private fun getLocalIpAddress(): String? {
+        try {
+
+            val wifiManager: WifiManager = getApplicationContext().getSystemService(WIFI_SERVICE) as WifiManager
+            return ipToString(wifiManager.connectionInfo.ipAddress)
+        } catch (ex: Exception) {
+            Log.e("IP Address", ex.toString())
+        }
+
+        return null
+    }
+
+    private fun ipToString(i: Int): String {
+        return (i and 0xFF).toString() + "." +
+                (i shr 8 and 0xFF) + "." +
+                (i shr 16 and 0xFF) + "." +
+                (i shr 24 and 0xFF)
+
+    }
+
 }
+

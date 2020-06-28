0e5fa1c94c7c3c2c69f8b469f2d309bb8b140dac
pavan <sairam.g581@gmail.com>
2020-06-28 13:26:54 +0530

    Added GraphView

--- Projects/MyApp/app/src/main/java/com/sairam/myapp/MainActivity.kt
+++ Projects/MyApp/app/src/main/java/com/sairam/myapp/MainActivity.kt
@@ -5,35 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.DataPointInterface
+import com.jjoe64.graphview.series.LineGraphSeries
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var series: LineGraphSeries<DataPoint>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text = findViewById<TextView>(R.id.textView)
-        text.text = getLocalIpAddress()
+        val graph: GraphView = findViewById<GraphView>(R.id.graph)
+        series = LineGraphSeries(arrayOf(
+            DataPoint(0.0,1.0),
+            DataPoint(1.0,5.0),
+            DataPoint(2.0,3.0),
+            DataPoint(3.0,2.0),
+            DataPoint(4.0,6.0)
+        ))
 
-    }
+        graph.addSeries(series)
 
-    private fun getLocalIpAddress(): String? {
-        try {
-
-            val wifiManager: WifiManager = getApplicationContext().getSystemService(WIFI_SERVICE) as WifiManager
-            return ipToString(wifiManager.connectionInfo.ipAddress)
-        } catch (ex: Exception) {
-            Log.e("IP Address", ex.toString())
-        }
-
-        return null
-    }
-
-    private fun ipToString(i: Int): String {
-        return (i and 0xFF).toString() + "." +
-                (i shr 8 and 0xFF) + "." +
-                (i shr 16 and 0xFF) + "." +
-                (i shr 24 and 0xFF)
 
     }
 

0fff2c8490dbefbee4c381bc3ea0b665b0dff2ab
pavan <sairam.g581@gmail.com>
2020-06-29 13:16:36 +0530

    Added function to get battery percentage

--- Projects/MyApp/app/src/main/java/com/sairam/myapp/MainActivity.kt
+++ Projects/MyApp/app/src/main/java/com/sairam/myapp/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.sairam.myapp
 
-import android.net.wifi.WifiManager
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.DataPointInterface
 import com.jjoe64.graphview.series.LineGraphSeries
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +19,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val graph: GraphView = findViewById<GraphView>(R.id.graph)
-        //graph.gridLabelRenderer.verticalAxisTitle = ""
+        val textView: TextView = findViewById(R.id.textView)
+
+
+        textView.text = "BatteryPercentage:${getBatteryPer()}"
+
+        val graph: GraphView = findViewById(R.id.graph)
+        graph.gridLabelRenderer.verticalAxisTitle   = "Percentage"
+        graph.gridLabelRenderer.horizontalAxisTitle = "Time"
         series = LineGraphSeries(arrayOf(
             DataPoint(0.0,1.0),
             DataPoint(1.0,5.0),
@@ -32,6 +38,17 @@ class MainActivity : AppCompatActivity() {
         graph.addSeries(series)
 
 
+    }
+
+    private fun getBatteryPer(): Float? {
+        val iFilter: IntentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        val batteryStatus: Intent? = registerReceiver(null,iFilter)
+        val batteryPer: Float? = batteryStatus?.let { intent ->
+            val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            level * 100 / scale.toFloat()
+        }
+        return batteryPer
     }
 
 }

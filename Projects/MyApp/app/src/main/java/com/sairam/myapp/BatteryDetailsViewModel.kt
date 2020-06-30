917832b832edd63d688d3e8f13b21245d9d2df31
pavan <sairam.g581@gmail.com>
2020-06-30 18:07:46 +0530

    Back to normal

--- Projects/MyApp/app/src/main/java/com/sairam/myapp/BatteryDetailsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.sairam.myapp
-
-import androidx.lifecycle.ViewModel
-
-class BatteryDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- Projects/MyApp/app/src/main/java/com/sairam/myapp/BatteryGraphViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.sairam.myapp
-
-import androidx.lifecycle.ViewModel
-
-class BatteryGraphViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
